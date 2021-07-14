b025758b435a26f73fb3ebf42b67817718cf41cd
Alaamansourn <mansouralaa887@gmail.com>
2021-07-14 11:39:06 +0300

    commit

---
+++ React/alaaprop/src/App.js
@@ -0,0 +1,19 @@
+import logo from './logo.svg';
+import './App.css';
+import Card from './components/Card.js'
+import React, { Component } from 'react';
+
+class App extends Component {
+  render() {
+    return (
+      <div className="App">
+        <Card firstName={"Doe"} lastName={"Jane"} age={45} hairColor={"Black"} />
+        <Card firstName={"Smith"} lastName={"John"} age={88} hairColor={"Brown"} />
+        <Card firstName={"Fillmore"} lastName={"Millard"} age={50} hairColor={"Brown"} />
+        <Card firstName={"Smith"} lastName={"Maria"} age={62} hairColor={"Brown"} />
+      </div>
+    );
+  }
+}
+
+export default App;
---
+++ React/alaaprop/src/components/Card.js
@@ -0,0 +1,15 @@
+import React, { Component } from 'react';
+
+class Card extends React.Component {
+    render() {
+        const { firstName, lastName, age, hairColor } = this.props;
+        return (
+            <div>
+                <h2>{firstName} , {lastName}</h2>
+                <p>age : {age}</p>
+                <p>hair color : {hairColor}</p>
+            </div>
+        );
+    }
+}
+export default Card;
